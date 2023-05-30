0ccac80e3c6f572dcf25fc8e14d9f791022781c4
rajeswar-net <39381511+rajeswar-net@users.noreply.github.com>
2023-05-30 16:43:25 -0500

    1 file modified

--- index.js
+++ index.js
@@ -14,10 +14,47 @@ let user = {
   }
 };
 
+// function properties are not supported in structuredClone, but circular references supported.
 let clone = structuredClone(user);
 
-alert( user.sizes === clone.sizes ); // false, different objects
+//alert( user.sizes === clone.sizes ); // false, different objects
 
 // user and clone are totally unrelated now
 user.sizes.width = 60;    // change a property from one place
-alert(clone.sizes.width); // 50, not related
+//alert(clone.sizes.width); // 50, not related
+
+
+let calculator = {
+
+  read() {
+    this.a = +prompt("a", this.a);
+    this.b = +prompt("b", this.b);
+  },
+  sum() {
+    return this.a+this.b;
+  },
+  mul() {
+    return this.a*this.b;
+  }
+};
+
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+
+let id = Symbol("id");
+alert(id.description); // id
+
+let id2 = Symbol("id");
+alert(id2.description); // id
+
+
+let user1 = { // belongs to another code
+  name: "John"
+};
+
+let id3 = Symbol("id");
+
+user1[id] = 1;
+
+alert( user1[id] ); // we can access the data using the symbol as the key
